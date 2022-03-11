2d32724012b84bb46a392a4bc13cb44a4bbb5b6b
Louis-Olivier Brassard <louis@loupbrun.ca>
2022-03-11 08:57:31 -0500

    un peu de personnalisation

--- sveltekit/src/lib/components/Navbar.svelte
+++ sveltekit/src/lib/components/Navbar.svelte
@@ -12,16 +12,26 @@
 </script>
 
 <Header
-  company="COCH"
-  platformName="Résultats Invitation 2022"
+  company={APP_CONFIG.company}
   href="/"
   bind:isSideNavOpen
 >
+  <span slot="platform">
+    {APP_CONFIG.name}
+    <code class="version">{APP_CONFIG.version}</code>
+  </span>
   <svelte:fragment slot="skip-to-content">
     <SkipToContent />
   </svelte:fragment>
   <HeaderNav>
-    <HeaderNavItem href="/" text="À propos" />
+    <HeaderNavItem href="/about" text="À propos" />
     <HeaderNavItem href={APP_CONFIG.hytekFtpLocation + 'index.htm'} text="Retour à l’interface classique" target="_blank" />
   </HeaderNav>
 </Header>
+
+<style>
+  .version {
+    font-weight: 300;
+    font-size: 90%;
+  }
+</style>
---
+++ sveltekit/src/routes/about.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { APP_CONFIG } from '../config';
+  import Navbar from '$lib/components/Navbar.svelte';
+  import Main from '$lib/components/Main.svelte';
+</script>
+  
+<Navbar />
+  
+<Main>
+  <h2>À propos de cette interface</h2>
+  <p>
+    Cette interface vise à offrir une meilleure expérience des résultats HyTek, notamment sur les appareils mobiles. Elle est développée par <a href="mailto:louis@corsaire-chaparral.org">Louis-Olivier Brassard</a> du club d’athlétisme Corsaire-Chaparral.
+  </p>
+</Main>
+
+<style>
+  h2 {
+    margin-bottom: 1rem;
+  }
+</style>
