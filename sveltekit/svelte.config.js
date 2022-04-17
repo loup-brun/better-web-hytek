7c93bf7d9ece197f184cf174c10f0358baec8963
Louis-Olivier Brassard <louis@loupbrun.ca>
2022-04-17 09:15:30 -0400

    tinker with adapter-static

--- sveltekit/svelte.config.js
+++ sveltekit/svelte.config.js
@@ -1,10 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      fallback: 'index.html',
+    }),
+    // we don’t want to prerender, since index.htm (and alike) are fetched at runtime
+    //prerender: {
+    //  default: true,
+    //},
+
+    paths: {
+      base: '/resultats/live'
+    },
   },
   preprocess: preprocess({
 
