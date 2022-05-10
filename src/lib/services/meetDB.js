171b36faeacbd37cbc09ef6f123e2c0c012d05e3
Louis-Olivier Brassard <louis@loupbrun.ca>
2022-05-10 10:58:18 -0400

    hook up meet db for / route

--- src/lib/services/meetDB.js
+++ src/lib/services/meetDB.js
@@ -2,22 +2,30 @@ export const db = new Map();
 
 // set values w/ meetId as key
 
-db.set('coch-inv-2022', {
-  title: 'Corsaire-Chaparral Invitation 2022',
+
+db.set('rseq-ll-primaire-2022', {
+  dateStart: new Date('2022-05-25'),
+  dateStart: new Date('2022-05-25'),
+  title: 'Mini-athlétisme RSEQ-LL',
   hytekFtpLocation: 'https://sherbrooke.rsstiming.com/ftp/220319/',
   hytekHtmlEncoding: 'ISO-8859-15',
   location: 'Stade d’athlétisme Richard-Garneau (Ste-Thérèse, QC)',
 });
 
 db.set('rseq-ll-2022', {
+  dateStart: new Date('2022-05-26'),
+  dateStart: new Date('2022-05-26'),
   title: 'CRS d’athlétisme RSEQ Laurentides-Lanaudière',
   hytekFtpLocation: 'https://sherbrooke.rsstiming.com/ftp/220319/',
   hytekHtmlEncoding: 'ISO-8859-15',
   location: 'Stade d’athlétisme Richard-Garneau (Ste-Thérèse, QC)',
 });
 
-db.set('rseq-ll-primaire-2022', {
-  title: 'Mini-athlétisme RSEQ-LL',
+
+db.set('coch-inv-2022', {
+  dateStart: new Date('2022-05-29'),
+  dateStart: new Date('2022-05-29'),
+  title: 'Corsaire-Chaparral Invitation 2022',
   hytekFtpLocation: 'https://sherbrooke.rsstiming.com/ftp/220319/',
   hytekHtmlEncoding: 'ISO-8859-15',
   location: 'Stade d’athlétisme Richard-Garneau (Ste-Thérèse, QC)',
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,34 @@
+<script context="module">
+  import { db } from '$lib/services/meetDB';
+
+  export function load() {
+    let events = db;
+
+    return {
+      props: {
+        events
+      }
+    }
+  }
+</script>
+
+<script>
+  // props
+  export let events;
+
+  // vars
+  let dbEvents = [... events.entries()];
+</script>
 <h1>Accueil</h1>
 
 <h2>Meets</h2>
 
 <ul>
+  {#each dbEvents as event}
   <li>
     <a
-      href="/meets/corsaire-chaparral-invitation-2022/hytek"
-    >COCH Invitation</a>
+      href="/meets/{event[0]}"
+    >{event[1].title}</a>
   </li>
+  {/each}
 </ul>
