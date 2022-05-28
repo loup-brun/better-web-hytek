ad8cd43827d89113d97eb6e20286331ac8d64c70
Louis-Olivier Brassard <louis@loupbrun.ca>
2022-05-28 08:43:41 -0400

    améliorer accueil

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,22 +13,50 @@
 </script>
 
 <script>
+  import Icon from '$lib/components/Icon.svelte';
+  import Navbar from '$lib/components/Navbar.svelte';
+
   // props
   export let events;
 
   // vars
-  let dbEvents = [... events.entries()];
+  let dbEvents = [... events.entries()]; // copy array
 </script>
-<h1>Accueil</h1>
 
-<h2>Meets</h2>
+<div class="Accueil">
+  <Navbar>
+    <h1 class="Accueil__titre | px-2 py-2 text-xl uppercase mx-auto">Résultats en ligne</h1>
+  </Navbar>
 
-<ul>
-  {#each dbEvents as event}
-  <li>
-    <a
-      href="/meets/{event[0]}"
-    >{event[1].title}</a>
-  </li>
-  {/each}
-</ul>
+  <ul class="Accueil__liste | border-b border-zinc-300">
+    {#each dbEvents as event}
+    <li class="Accueil__item">
+      <a
+        href="/meets/{event[0]}"
+        class="Accueil__lien | flex flex-row nowrap justify-between items-center px-2 py-3 border-t border-zinc-300 hover:bg-zinc-100"
+      >
+        <div class="flex-grow-0 overflow-hidden text-ellipsis">
+          <span class="Accueil__item-date | hidden md:inline-block">
+            {event[1].dateStart}
+          </span>
+          <span class="Accueil__item-titre | font-bold">
+            {event[1].title}
+          </span>
+        </div>
+
+        <Icon name="chevronRight" />
+      </a>
+    </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .Accueil__liste {
+
+  }
+  .Accueil__lien {
+
+  }
+
+</style>
