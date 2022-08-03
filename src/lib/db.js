0c98002c94bf3e931dee588db706f00f3dce111f
Louis-Olivier Brassard <louis@loupbrun.ca>
2022-08-03 09:40:59 -0400

    quebec: ajout soiree rouge et orange
    et préparer soirée r&o #2 2022

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,38 @@
 export const meets = [
+
+  {
+    id: 'soiree-rouge-orange-2022',
+
+    // meet info
+    dateStart: '2022-07-16',
+    dateEnd: '2022-07-16',
+    title: 'Soirée Rouge et Orange',
+    stadiumName: 'Stade de l’UQTR',
+    address: '3351 boul. des Forges',
+    address2: '',
+    city: 'Trois-Rivières',
+    province: 'QC',
+    postalCode: '',
+
+    // résultats
+    resultsType: 'hytek',
+    finishlynx: true,
+    hytekFtpLocation: 'https://rsstiming.com/quebec/220716/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    sessionNames: [
+      'Épreuves de piste',
+    ],
+
+    // personnalisation
+    logo: '/logos/patriotes-uqtr.png',
+    themeColor: '#2D7944',
+
+    // credits
+    timingCompany: 'CAUL',
+    timingCompanyUrl: 'https://www.caul.ca/',
+    hostingProvider: 'RSS Timing',
+    hostingProviderUrl: 'https://rsstiming.com',
+  },
   {
     id: 'prov-civil-jun-sen-para-2022',
 
@@ -34,5 +68,41 @@ export const meets = [
     timingCompanyUrl: 'https://www.caul.ca/',
     hostingProvider: 'RSS Timing',
     hostingProviderUrl: 'https://rsstiming.com',
-  }
+  },
+  // {
+  //   id: 'soiree-2-2022',
+  //
+  //   // meet info
+  //   dateStart: '2022-08-15',
+  //   dateEnd: '2022-08-15',
+  //   title: 'Soirée Rouge et Or #2',
+  //   stadiumName: 'PEPS de l’Université Laval',
+  //   address: '2380 Rue du Peps',
+  //   address2: '',
+  //   city: 'Québec',
+  //   province: 'QC',
+  //   postalCode: 'G1V 0A6',
+  //
+  //   // résultats
+  //   resultsType: 'hytek',
+  //   finishlynx: true,
+  //   hytekFtpLocation: 'https://rsstiming.com/quebec/220815/',
+  //   hytekHtmlEncoding: 'ISO-8859-15',
+  //   // sessionNames: [
+  //   //   'Samedi piste',
+  //   //   'Samedi concours',
+  //   //   'Dimanche piste',
+  //   //   'Dimanche concours',
+  //   // ],
+  //
+  //   // personnalisation
+  //   logo: '/apple-touch-icon.png',
+  //   themeColor: '#e0181c',
+  //
+  //   // credits
+  //   timingCompany: 'CAUL',
+  //   timingCompanyUrl: 'https://www.caul.ca/',
+  //   hostingProvider: 'RSS Timing',
+  //   hostingProviderUrl: 'https://rsstiming.com',
+  // },
 ];
