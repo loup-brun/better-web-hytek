8447c06bb1891646017a45cedc0d441ea10bca85
Louis-Olivier Brassard <louis@loupbrun.ca>
2022-08-09 20:09:28 -0400

    db: dé-commenter soirée r&o 2022

--- src/lib/db.js
+++ src/lib/db.js
@@ -70,40 +70,40 @@ export const meets = [
     hostingProvider: 'RSS Timing',
     hostingProviderUrl: 'https://rsstiming.com',
   },
-  // {
-  //   id: 'soiree-2-2022',
-  //
-  //   // meet info
-  //   dateStart: '2022-08-15',
-  //   dateEnd: '2022-08-15',
-  //   title: 'Soirée Rouge et Or #2',
-  //   stadiumName: 'PEPS de l’Université Laval',
-  //   address: '2380 Rue du Peps',
-  //   address2: '',
-  //   city: 'Québec',
-  //   province: 'QC',
-  //   postalCode: 'G1V 0A6',
-  //
-  //   // résultats
-  //   resultsType: 'hytek',
-  //   finishlynx: true,
-  //   hytekFtpLocation: 'https://rsstiming.com/quebec/220815/',
-  //   hytekHtmlEncoding: 'ISO-8859-15',
-  //   // sessionNames: [
-  //   //   'Samedi piste',
-  //   //   'Samedi concours',
-  //   //   'Dimanche piste',
-  //   //   'Dimanche concours',
-  //   // ],
-  //
-  //   // personnalisation
-  //   logo: '/apple-touch-icon.png',
-  //   themeColor: '#e0181c',
-  //
-  //   // credits
-  //   timingCompany: 'CAUL',
-  //   timingCompanyUrl: 'https://www.caul.ca/',
-  //   hostingProvider: 'RSS Timing',
-  //   hostingProviderUrl: 'https://rsstiming.com',
-  // },
+  {
+    id: 'soiree-2-2022',
+
+    // meet info
+    dateStart: '2022-08-15',
+    dateEnd: '2022-08-15',
+    title: 'Soirée Rouge et Or #2',
+    stadiumName: 'PEPS de l’Université Laval',
+    address: '2380 Rue du Peps',
+    address2: '',
+    city: 'Québec',
+    province: 'QC',
+    postalCode: 'G1V 0A6',
+
+    // résultats
+    resultsType: 'hytek',
+    finishlynx: true,
+    hytekFtpLocation: 'https://rsstiming.com/quebec/220815/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    // sessionNames: [
+    //   'Samedi piste',
+    //   'Samedi concours',
+    //   'Dimanche piste',
+    //   'Dimanche concours',
+    // ],
+
+    // personnalisation
+    logo: '/apple-touch-icon.png',
+    themeColor: '#e0181c',
+
+    // credits
+    timingCompany: 'CAUL',
+    timingCompanyUrl: 'https://www.caul.ca/',
+    hostingProvider: 'RSS Timing',
+    hostingProviderUrl: 'https://rsstiming.com',
+  },
 ];
