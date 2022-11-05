27f0ea0e0ae28b9d0b8516ef1e3c1297716a9732
Louis-Olivier Brassard <louis@loupbrun.ca>
2022-11-05 16:02:33 -0400

    db: add mcgill 2022

--- src/lib/db.js
+++ src/lib/db.js
@@ -111,7 +111,7 @@ export const meets = [
     // credits
     timingCompany: 'COCH Chrono',
     hostingProvider: 'Corsaire-Chaparral.org',
-    hostingProviderUrl: 'https://static.resultats.corsaire-chaparral.org',
+    hostingProviderUrl: 'https://www.corsaire-chaparral.org',
   },
 
   {
@@ -177,6 +177,38 @@ export const meets = [
     // credits
     timingCompany: 'COCH Chrono',
     hostingProvider: 'Corsaire-Chaparral.org',
-    hostingProviderUrl: 'https://static.resultats.corsaire-chaparral.org',
-  }
+    hostingProviderUrl: 'https://www.corsaire-chaparral.org',
+  },
+
+  {
+    id: 'mcgill-martlet-2022',
+    dateStart: '2022-11-26',
+    dateEnd: '2022-11-26',
+    title: 'McGill Martlet 2022',
+    stadiumName: 'Tomlinson Fieldhouse',
+    address: '475 avenue des Pins',
+    address2: '',
+    city: 'Montréal',
+    province: 'QC',
+    postalCode: '',
+
+    // résultats
+    resultsType: 'hytek',
+    hytekFtpLocation: 'https://static.resultats.corsaire-chaparral.org/20221126/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    sessionNames: [
+      'Pelouse / Field',
+      'Piste / Track',
+    ],
+
+    // personnalisation
+    logo: 'https://dbukjj6eu5tsf.cloudfront.net/sidearm.sites/mcgill.sidearmsports.com/images/responsive_2020/splash-logo.svg',
+    // logoMaxWidth: 224,
+    themeColor: '#ED1B2F',
+
+    // credits
+    timingCompany: 'COCH Chrono',
+    hostingProvider: 'Corsaire-Chaparral.org',
+    hostingProviderUrl: 'https://www.corsaire-chaparral.org',
+  },
 ];
