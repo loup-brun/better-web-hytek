8ab8275484ab42a60d15fc737416c5fb14b2b2b2
Louis-Olivier Brassard <louis@loupbrun.ca>
2023-01-20 09:17:41 -0500

    db: add mcgill team challenge 2023

--- src/lib/db.js
+++ src/lib/db.js
@@ -31,4 +31,39 @@ export const meets = [
     //hostingProvider: 'Corsaire-Chaparral.org',
     hostingProviderUrl: 'https://www.corsaire-chaparral.org',
   },
+  {
+    id: 'mcgill-team-challenge-2023',
+    dateStart: '2023-01-27',
+    dateEnd: '2023-01-28',
+    title: 'McGill Team Challenge 2023',
+    stadiumName: 'Tomlinson Fieldhouse',
+    address: '475 avenue des Pins',
+    address2: '',
+    city: 'Montréal',
+    province: 'QC',
+    postalCode: '',
+
+    // résultats
+    resultsType: 'hytek',
+    hytekFtpLocation: 'https://rsstiming.com/mcgill/230127/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    sessionNames: [
+      'Session #1',
+      'Session #2',
+      'Session #3',
+      'Session #4',
+    ],
+
+    // personnalisation
+    logo: 'https://dbukjj6eu5tsf.cloudfront.net/sidearm.sites/mcgill.sidearmsports.com/images/responsive_2020/splash-logo.svg',
+    // logoMaxWidth: 224,
+    themeColor: '#ED1B2F',
+
+    // credits
+    timingCompany: 'RSS Timing',
+    timingCompanyUrl: 'https://rsstiming.com',
+    hostingProvider: 'RSS Timing',
+    hostingProviderUrl: 'https://rsstiming.com',
+  },
+
 ];
