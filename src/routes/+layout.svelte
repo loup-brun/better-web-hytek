9d8b462cdc792a468e62dd9da2da5d34799189f3
Louis-Olivier Brassard <louis@loupbrun.ca>
2023-02-24 22:08:52 -0500

    vercel analytics: move to root +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
   import '$lib/styles/app.pcss';
+  // vercel analytics
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
