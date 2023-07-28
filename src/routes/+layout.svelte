fe7ff72f19daea04949b1ef57c20410337f9d67e
Louis-Olivier Brassard <louis@loupbrun.ca>
2023-07-28 18:19:57 -0400

    remove vercel analytics
    introduced in commit 0578c57ac1cfe20ef1f22b94cebb8d73bf60d7b9

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script>
   import '$lib/styles/app.pcss';
-  // vercel analytics
-  import { dev } from '$app/environment';
-  import { inject } from '@vercel/analytics';
-
-  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
