7f8e6dbd6e805b5fe97c545dda38c92cca936e09
Louis-Olivier Brassard <louis@loupbrun.ca>
2023-12-14 21:50:10 -0500

    quebec: update db (meet 15 déc. 2023)

--- src/lib/db.js
+++ src/lib/db.js
@@ -353,4 +353,40 @@ export const meets = [
     hostingProvider: 'RSS Timing',
     hostingProviderUrl: 'https://rsstiming.com',
   },
+
+  {
+    id: '231215',
+
+    // meet info
+    dateStart: '2023-12-15',
+    dateEnd: '2023-12-15',
+    title: 'Crépuscule hivernal',
+    stadiumName: 'PEPS de l’Université Laval',
+    address: '2380 Rue du Peps',
+    address2: '',
+    city: 'Québec',
+    province: 'QC',
+    postalCode: 'G1V 0A6',
+
+    // résultats
+    resultsType: 'hytek',
+    finishlynx: true,
+    hytekFtpLocation: 'https://rsstiming.com/quebec/231215/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    sessionNames: [
+      'Concours',
+      'Piste',
+    ],
+
+    // personnalisation
+    logo: '/apple-touch-icon.png',
+    themeColor: '#e0181c',
+
+    // credits
+    timingCompany: 'CAUL',
+    timingCompanyUrl: 'https://www.caul.ca/',
+    hostingProvider: 'RSS Timing',
+    hostingProviderUrl: 'https://rsstiming.com',
+  },
+
 ];
