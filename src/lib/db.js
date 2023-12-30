10a091388b41eb2343c771ea08e71e9e9cf74147
Louis-Olivier Brassard <louis@loupbrun.ca>
2023-12-29 19:42:52 -0500

    quebec: update db, ajout logo athletisme-qc

--- src/lib/db.js
+++ src/lib/db.js
@@ -324,7 +324,7 @@ export const meets = [
     // meet info
     dateStart: '2023-12-03',
     dateEnd: '2023-12-03',
-    title: 'Championnat scolaire #1 - 3 décembre',
+    title: 'Championnat scolaire #1',
     stadiumName: 'PEPS de l’Université Laval',
     address: '2380 Rue du Peps',
     address2: '',
@@ -381,6 +381,154 @@ export const meets = [
     // personnalisation
     logo: '/apple-touch-icon.png',
     themeColor: '#e0181c',
+
+    // credits
+    timingCompany: 'CAUL',
+    timingCompanyUrl: 'https://www.caul.ca/',
+    hostingProvider: 'RSS Timing',
+    hostingProviderUrl: 'https://rsstiming.com',
+  },
+
+  {
+    id: '240114',
+
+    // meet info
+    dateStart: '2024-01-14',
+    dateEnd: '2024-01-14',
+    title: 'Championnat scolaire #2',
+    stadiumName: 'PEPS de l’Université Laval',
+    address: '2380 Rue du Peps',
+    address2: '',
+    city: 'Québec',
+    province: 'QC',
+    postalCode: 'G1V 0A6',
+
+    // résultats
+    resultsType: 'hytek',
+    finishlynx: true,
+    hytekFtpLocation: 'https://rsstiming.com/quebec/240114/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    // sessionNames: [
+    //   'Concours',
+    //   'Piste',
+    // ],
+
+    // personnalisation
+    logo: '/logos/rseq.png',
+    logoMaxWidth: 128,
+    themeColor: '#2C99D8',
+
+    // credits
+    timingCompany: 'CAUL',
+    timingCompanyUrl: 'https://www.caul.ca/',
+    hostingProvider: 'RSS Timing',
+    hostingProviderUrl: 'https://rsstiming.com',
+  },
+
+  {
+    id: 'championnat-universitaire-2023',
+
+    // meet info
+    dateStart: '2024-02-23',
+    dateEnd: '2024-02-24',
+    title: 'Championnat provincial universitaire 2024',
+    stadiumName: 'PEPS de l’Université Laval',
+    address: '2380 Rue du Peps',
+    address2: '',
+    city: 'Québec',
+    province: 'QC',
+    postalCode: 'G1V 0A6',
+
+    // résultats
+    resultsType: 'hytek',
+    finishlynx: true,
+    hytekFtpLocation: 'https://rsstiming.com/quebec/240223/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    // sessionNames: [
+    //   'Concours samedi',
+    //   'Piste samedi',
+    //   'Concours dimanche',
+    //   'Piste dimanche',
+    // ],
+
+    // personnalisation
+    logo: '/logos/rseq.png',
+    logoMaxWidth: 128,
+    themeColor: '#2C99D8',
+
+    // credits
+    timingCompany: 'CAUL',
+    timingCompanyUrl: 'https://www.caul.ca/',
+    hostingProvider: 'RSS Timing',
+    hostingProviderUrl: 'https://rsstiming.com',
+  },
+
+  {
+    id: '240225',
+
+    // meet info
+    dateStart: '2024-02-25',
+    dateEnd: '2024-02-25',
+    title: 'Championnat scolaire #3',
+    stadiumName: 'PEPS de l’Université Laval',
+    address: '2380 Rue du Peps',
+    address2: '',
+    city: 'Québec',
+    province: 'QC',
+    postalCode: 'G1V 0A6',
+
+    // résultats
+    resultsType: 'hytek',
+    finishlynx: true,
+    hytekFtpLocation: 'https://rsstiming.com/quebec/240225/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    // sessionNames: [
+    //   'Concours',
+    //   'Piste',
+    // ],
+
+    // personnalisation
+    logo: '/logos/rseq.png',
+    logoMaxWidth: 128,
+    themeColor: '#2C99D8',
+
+    // credits
+    timingCompany: 'CAUL',
+    timingCompanyUrl: 'https://www.caul.ca/',
+    hostingProvider: 'RSS Timing',
+    hostingProviderUrl: 'https://rsstiming.com',
+  },
+
+  {
+    id: 'champ-qc-ensalle-2023',
+
+    // meet info
+    dateStart: '2024-03-02',
+    dateEnd: '2024-03-03',
+    title: 'Championnat québécois benjamin-cadet',
+    stadiumName: 'PEPS de l’Université Laval',
+    address: '2380 Rue du Peps',
+    address2: '',
+    city: 'Québec',
+    province: 'QC',
+    postalCode: 'G1V 0A6',
+
+    // résultats
+    resultsType: 'hytek',
+    finishlynx: true,
+    hytekFtpLocation: 'https://rsstiming.com/quebec/240302/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    // sessionNames: [
+    //   'Concours samedi',
+    //   'Piste samedi',
+    //   'Concours dimanche',
+    //   'Piste dimanche',
+    // ],
+
+    // personnalisation
+    logo: '/logos/athletisme-quebec-2023.svg',
+    logoMaxWidth: 112,
+    themeColor: '#00a1e4',
 
     // credits
     timingCompany: 'CAUL',
