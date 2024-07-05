bc7b9e0b910d45ec47e5a00feefc8868e1bbebd9
Louis-Olivier Brassard <louis@loupbrun.ca>
2024-07-05 09:56:48 -0400

    maj laval

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,72 @@
 export const meets = [
   {
+    id: 'crepuscule-laval-2022',
+    // meet info
+    dateStart: '2022-07-01',
+    dateEnd: '2022-07-01',
+    title: 'Crépuscule Dynamique de Laval 2022',
+    stadiumName: 'Stade d’athlétisme Claude-Ferragne',
+    address: '4600 Cyrille-Delage',
+    address2: '',
+    city: 'Laval',
+    province: 'QC',
+    postalCode: '',
+
+    // résultats
+    resultsType: 'hytek',
+    finishlynx: true,
+    hytekFtpLocation: 'https://laval.rsstiming.com/ftp/220701/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    sessionNames: [
+      'Concours',
+      'Piste'
+    ],
+
+    // personnalisation
+    logo: '/logos/athletisme-laval.png',
+    logoMaxWidth: 130,
+    themeColor: '#013871',
+
+    // credits
+    timingCompany: 'RSS Timing',
+    timingCompanyUrl: 'https://www.rsstiming.com/',
+  },
+  {
+    id: 'champ-qc-ben-cad-juv-2022',
+    // meet info
+    dateStart: '2022-07-02',
+    dateEnd: '2022-07-03',
+    title: 'Championnats québécois benjamins-cadets-juvéniles',
+    stadiumName: 'Stade d’athlétisme Claude-Ferragne',
+    address: '4600 Cyrille-Delage',
+    address2: '',
+    city: 'Laval',
+    province: 'QC',
+    postalCode: '',
+
+    // résultats
+    resultsType: 'hytek',
+    finishlynx: true,
+    hytekFtpLocation: 'https://laval.rsstiming.com/ftp/220702/',
+    hytekHtmlEncoding: 'ISO-8859-15',
+    sessionNames: [
+      'Concours samedi',
+      'Piste samedi',
+      'Piste dimanche',
+      'Concours dimanche',
+    ],
+
+    // personnalisation
+    logo: '/logos/athletisme-quebec.svg',
+    logoMaxWidth: 120,
+    themeColor: '#00a1e4',
+
+    // credits
+    timingCompany: 'RSS Timing',
+    timingCompanyUrl: 'https://www.rsstiming.com/',
+  },
+  
+  {
     id: 'jdq-laval-2022',
     // meet info
     dateStart: '2022-07-23',
